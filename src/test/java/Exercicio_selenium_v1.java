7f36f6c24977cc40f1e163cbe25d210d43ae80d7
keila <keila.simao@yahoo.com.br>
2022-05-14 15:35:11 -0300

    Exercicios completos

--- src/test/java/Exercicio_selenium_v1.java
+++ src/test/java/Exercicio_selenium_v1.java
@@ -1,15 +1,19 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-    public class Exercicio_selenium_v1 {
+public class Exercicio_selenium_v1 {
         WebDriver driver = new ChromeDriver();
 
 
         @Test
-        public void testeExercicosSelenium() {
+        public void testeExercicosSelenium() throws InterruptedException {
+            //Exercicio01
             driver.get("file:///C:/Users/Dell/OneDrive/Documentos/Registro%20web.html");
             driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
                     .sendKeys("Flavio");
@@ -18,7 +22,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
             driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
             driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
             driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+
+            //Exercicio 02
+            driver.findElement(By.xpath("//*[@value='Male']")).click();
+            Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Male']")).isSelected());
+
+            //Exercicio03
+            driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
+            Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
+            Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
+
+            //Exercicio 04
+
+            driver.findElement(By.xpath("//div[@id='msdd']")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
+
+
+            //Listas Option
+
+            //com Click, tem que navegar até o elemento option e extrair o xpath
+            driver.findElement(By.xpath("//option[@value='Java']")).click();
+            Thread.sleep(2000);
+
+            //Selecionar Lista Option/Select com Select Selenium
+            WebElement selectElement = driver.findElement(By.xpath("//select[@id='Skills']"));
+            Select select = new Select(selectElement);
+            select.selectByVisibleText("Linux");
+
+
+
         }
+
         @After
 
         public void after() throws InterruptedException {
