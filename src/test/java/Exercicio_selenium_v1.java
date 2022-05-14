50795bf0964ce6a7ed2db71fd8f9034702bb8b23
keila <keila.simao@yahoo.com.br>
2022-05-14 16:23:21 -0300

    Exercicio 4 completo

--- src/test/java/Exercicio_selenium_v1.java
+++ src/test/java/Exercicio_selenium_v1.java
@@ -12,45 +12,59 @@ public class Exercicio_selenium_v1 {
 
 
         @Test
-        public void testeExercicosSelenium() throws InterruptedException {
-            //Exercicio01
-            driver.get("file:///C:/Users/Dell/OneDrive/Documentos/Registro%20web.html");
-            driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
-                    .sendKeys("Flavio");
-            driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[@class='form-group']/div[2]/input[1]"))
-                    .sendKeys("Dias");
-            driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
-            driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
-            driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+            public void testeExercicosSelenium() throws InterruptedException {
+                //Exercicio01
+                driver.get("file:///C:/Users/Dell/OneDrive/Documentos/Registro%20web.html");
+                driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
+                        .sendKeys("Flavio");
+                driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[@class='form-group']/div[2]/input[1]"))
+                        .sendKeys("Dias");
+                driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
+                driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
+                driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
 
-            //Exercicio 02
-            driver.findElement(By.xpath("//*[@value='Male']")).click();
-            Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Male']")).isSelected());
+                //Exercicio 02
+                driver.findElement(By.xpath("//*[@value='Male']")).click();
+                Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Male']")).isSelected());
 
-            //Exercicio03
-            driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
-            Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
-            Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
+                //Exercicio03
+                driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
+                Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
+                Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
 
-            //Exercicio 04
+                //Exercicio 04
 
-            driver.findElement(By.xpath("//div[@id='msdd']")).click();
-            Thread.sleep(1000);
-            driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
-
-
-            //Listas Option
-
-            //com Click, tem que navegar até o elemento option e extrair o xpath
-            driver.findElement(By.xpath("//option[@value='Java']")).click();
-            Thread.sleep(2000);
-
-            //Selecionar Lista Option/Select com Select Selenium
-            WebElement selectElement = driver.findElement(By.xpath("//select[@id='Skills']"));
-            Select select = new Select(selectElement);
-            select.selectByVisibleText("Linux");
+                driver.findElement(By.xpath("//div[@id='msdd']")).click();
+                Thread.sleep(1000);
+                driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
 
 
+                //Listas Option
+
+                //com Click, tem que navegar até o elemento option e extrair o xpath
+                driver.findElement(By.xpath("//option[@value='Java']")).click();
+                Thread.sleep(2000);
+
+                //Selecionar Lista Option/Select com Select Selenium
+                WebElement selectElement = driver.findElement(By.xpath("//select[@id='Skills']"));
+                Select select = new Select(selectElement);
+                select.selectByVisibleText("Linux");
+
+            //Countries
+            driver.findElement(By.xpath("//span[@class='select2-selection select2-selection--single']")).click();
+            driver.findElement(By.xpath("//*[@class='select2-results__options']//*[contains(text(),'United States of America')]")).click();
+
+            // Birthday
+
+            driver.findElement(By.xpath("//option[@value='1988']")).click();
+            driver.findElement(By.xpath("//option[@value='April']")).click();
+            driver.findElement(By.xpath("//option[@value='15']")).click();
+
+            //Password
+
+            driver.findElement(By.xpath("//input[@id='firstpassword']")).sendKeys("teste123");
+            driver.findElement(By.xpath("//input[@id='secondpassword']")).sendKeys("teste1223");
+            driver.findElement(By.xpath("//button[@id='submitbtn']")).click();
 
         }
 
@@ -62,3 +76,4 @@ public class Exercicio_selenium_v1 {
         }
 
 }
+//body/section[@id='section']/div[1]/div[1]/div[2]/form[1]/div[10]/div[1]/span[1]/span[1]/span[1]
